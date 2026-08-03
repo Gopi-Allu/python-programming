71301cafabd585f5eedc27cd908da15930c8bc64
Gopi-Allu <chanduaklu@gmail.com>
2026-08-03 11:09:58 +0530

    Add files via upload

---
+++ concatenation.py
@@ -0,0 +1,4 @@
+member1=("roja",22,"dwarakanagar,ap","jntu-gv university")
+member2=("suraj",24,"visakhapatnam,ap","jntu-gv university")
+concatenated_tuple=member1+member2
+print("concatenated tuple:",concatenated_tuple)
---
+++ countvowels.py
@@ -0,0 +1,3 @@
+input_string="Hello,World!"
+vowel_count=sum(input_string.lower().count(vowel)for vowel in "aeiou")
+print("number of vowels:",vowel_count)
